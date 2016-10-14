43e52f11317f48699a4fedd96869a667b8723c49
最萌小汐 <sunyijiusyj@126.com>
2016-10-14 17:29:14 +0800

    生成工具

--- make.bat
+++ make.bat
@@ -1,3 +1,6 @@
 @echo off
+del "%~dp0\map\lua\currentpath.lua"
+echo return [[%~dp0script\]] >> "%~dp0\map\lua\currentpath.lua"
 "%~dp0w3x2txt\build\lua.exe" "%~dp0src\make.lua" "%~dp0\" "debug" %1
+del "%~dp0\map\lua\currentpath.lua"
 pause
--- release.bat
+++ release.bat
@@ -1,3 +1,5 @@
 @echo off
+del "%~dp0\map\lua\currentpath.lua"
 "%~dp0w3x2txt\build\lua.exe" "%~dp0src\make.lua" "%~dp0\" "release" %1
+del "%~dp0\map\lua\currentpath.lua"
 pause
