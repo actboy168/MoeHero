eb2f7b047b397faea2b9a521275e5b8866e17607
actboy168 <actboy168@gmail.com>
2016-10-15 23:06:45 +0800

    增加一个错误处理

--- tools/run.lua
+++ tools/run.lua
@@ -3,11 +3,17 @@ require 'filesystem'
 require 'registry'
 require 'sys'
 
-local rootpath = fs.get(fs.DIR_EXE):remove_filename():remove_filename():remove_filename()
-local command = (registry.current_user() / [[SOFTWARE\Classes\YDWEMap\shell\run_war3\command]])['']
-    :gsub('%%1', (rootpath / 'MoeHero.w3x'):string())
+local function main()
+    local rootpath = fs.get(fs.DIR_EXE):remove_filename():remove_filename():remove_filename()
+    local command = (registry.current_user() / [[SOFTWARE\Classes\YDWEMap\shell\run_war3\command]])['']
+        :gsub('%%1', (rootpath / 'MoeHero.w3x'):string())
 
-local p = sys.process()
-if p:create(nil, command, nil) then
-	p:close()
+    local p = sys.process()
+    if p:create(nil, command, nil) then
+    	p:close()
+    end
+end
+
+if not pcall(main) then
+    print('需要YDWE关联w3x文件')
 end
