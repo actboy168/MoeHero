16df512572eb5d053758757b917d3daab2fe1ce0
actboy168 <actboy168@gmail.com>
2016-10-18 11:04:20 +0800

    整理代码

---
+++ tools/bin/ydwe.lua
@@ -0,0 +1,15 @@
+require 'luabind'
+require 'filesystem'
+require 'registry'
+
+local function main()
+    local command = (registry.current_user() / [[SOFTWARE\Classes\YDWEMap\shell\run_war3\command]])['']
+    local f, l = command:find('"[^"]*"')
+    return fs.path(command:sub(f+1, l-1)):remove_filename():remove_filename()
+end
+local suc, r = pcall(main)
+if not suc or not r then
+    print('需要YDWE关联w3x文件')
+    return
+end
+ydwe = r
--- tools/运行.lua
+++ tools/运行.lua
@@ -1,18 +1,11 @@
-require 'luabind'
-require 'filesystem'
-require 'registry'
+require 'ydwe'
 require 'sys'
-
-local function main()
-    local rootpath = fs.get(fs.DIR_EXE):remove_filename():remove_filename():remove_filename()
-    local command = (registry.current_user() / [[SOFTWARE\Classes\YDWEMap\shell\run_war3\command]])['']
-        : gsub('%%1', (rootpath / 'MoeHero.w3x'):string())
-    local p = sys.process()
-    if p:create(nil, command, nil) then
-    	p:close()
-    end
+if not ydwe then
+    return
 end
-
-if not pcall(main) then
-    print('需要YDWE关联w3x文件')
+local rootpath = fs.get(fs.DIR_EXE):remove_filename():remove_filename():remove_filename()
+local command = ([["%s" -launchwar3 -loadfile "%s"]]):format((ydwe / 'bin' / 'ydweconfig.exe'):string(), (rootpath / 'MoeHero.w3x'):string())
+local p = sys.process()
+if p:create(nil, command, nil) then
+    p:close()
 end
