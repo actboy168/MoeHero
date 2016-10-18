7dd64de3d1429d7577e515ff2fa3120cdc510e24
最萌小汐 <sunyijiusyj@126.com>
2016-10-18 14:32:07 +0800

    支持新积分

--- script/types/record.lua
+++ script/types/record.lua
@@ -1,76 +1,66 @@
 local japi = require 'jass.japi'
 local jass = require 'jass.common'
 
-if not japi.InitGameCache then
-	local names = {
-		'InitGameCache',
-		'StoreInteger',
-		'GetStoredInteger',
-		'StoreString',
-		'SaveGameCache',
-	}
-	for _, name in ipairs(names) do
+local names = {
+	'FlushGameCache',
+	'InitGameCache',
+	'StoreInteger',
+	'GetStoredInteger',
+	'StoreString',
+	'SaveGameCache',
+	'SyncStoredInteger',
+}
+for _, name in ipairs(names) do
+	if not japi[name] then
 		rawset(japi, name, jass[name])
 	end
+end
+
+local function get_key(player)
+	return string.char(('A'):byte() + (player:get() - 1))
 end
 
 --获取积分对象
 function ac.player.__index:record()
 	if not self.record_data then
-		if self:isPlayer() then
-			self.record_data = japi.InitGameCache('11SAV@' .. string.char(('A'):byte() + (self:get() - 1)))
+		if self:is_player() then
+			self.record_data = japi.InitGameCache('11SAV@' .. get_key(self))
 		else
 			self.record_data = japi.InitGameCache('')
 		end
 	end
 	return self.record_data
 end
-
-local keys = {}
-local function get_key(name)
-	if not keys[name] then
-		if ac.game.new_record then
-			keys[name] = name
-		else
-			keys[name] = name:match '^(.*)-%d*$' or name
-		end
-	end
-	return keys[name]
-end
 	
 --设置积分显示
 -- 位置[0-7]
 -- 名称
 function ac.game:record(n, name)
-	local key = get_key(name)
 	for i = 1, 12 do
-		japi.StoreString(ac.player(i):record(), '', 'Title@' .. string.char(('A'):byte() + n), key)
+		japi.StoreString(ac.player(i):record(), '', 'Title@' .. string.char(('A'):byte() + n), name)
 	end
-	log.info(('设置积分标题[%d]为[%s]'):format(n, key))
+	log.info(('设置积分标题[%d]为[%s]'):format(n, name))
 end
 
 --获取积分
 function ac.player.__index:get_record(name)
-	local key = get_key(name)
-	local value = japi.GetStoredInteger(self:record(), '', key)
-	log.info(('获取积分:[%s][%s] --> [%d]'):format(self:get_name(), key, value))
+	local value = japi.GetStoredInteger(self:record(), '', name)
+	log.info(('获取积分:[%s][%s] --> [%d]'):format(self:get_name(), name, value))
 	return value
 end
 
 --设置积分
 function ac.player.__index:set_record(name, value)
-	local key = get_key(name)
-	log.info(('设置积分:[%s][%s] <-- [%d]'):format(self:get_name(), key, value))
-	return japi.StoreInteger(self:record(), '', key, value)
+	log.info(('设置积分:[%s][%s] <-- [%d]'):format(self:get_name(), name, value))
+	return japi.StoreInteger(self:record(), '', name, value)
 end
 
 --增加积分
 function ac.player.__index:add_record(name, value)
-	local key = get_key(name)
-	local old_value = japi.GetStoredInteger(self:record(), '', key)
+	local old_value = japi.GetStoredInteger(self:record(), '', name)
 	local new_value = old_value + value
-	log.info(('增加积分:[%s][%s] : [%d] --> [%d]'):format(self:get_name(), key, old_value, new_value))
-	return japi.StoreInteger(self:record(), '', key, new_value)
+	log.info(('增加积分:[%s][%s] : [%d] --> [%d]'):format(self:get_name(), name, old_value, new_value))
+	return japi.StoreInteger(self:record(), '', name, new_value)
 end
 
 --保存积分
@@ -78,3 +68,58 @@ function ac.player.__index:save_record()
 	log.info(('保存积分:[%s]'):format(self:get_name()))
 	return japi.SaveGameCache(self:record())
 end
+
+-- RPG积分相关
+local score_gc
+local function get_score()
+	if not score_gc then
+		japi.FlushGameCache(japi.InitGameCache("11.x"))
+		score_gc = japi.InitGameCache("11.x")
+	end
+	return score_gc
+end
+
+local current_player
+local function get_player()
+	if current_player and current_player:is_player() then
+		return current_player
+	end
+	for i = 1, 12 do
+		if ac.player[i]:is_player() then
+			current_player = ac.player[i]
+			return current_player
+		end
+	end
+	return ac.player[1]
+end
+
+local function write_score(table, key, data)
+	japi.StoreInteger(get_score(), table, key, data)
+	if get_player():is_self() then
+		japi.SyncStoredInteger(gc, table, key)
+	end
+end
+
+local function read_score(table, key)
+	return japi.GetStoredInteger(get_score(), table, key)
+end
+
+function ac.player.__index:get_score(name)
+	local value = read_score(get_key(self), name)
+	log.info(('获取RPG积分:[%s][%s] --> [%d]'):format(self:get_name(), name, value))
+	return value
+end
+
+function ac.player.__index:set_score(name, value)
+	log.info(('设置积分:[%s][%s] <-- [%d]'):format(self:get_name(), name, value))
+	write_score(get_key(self) .. "=", name, value)
+end
+
+function ac.player.__index:add_scrore(name, value)
+	log.info(('增加积分:[%s][%s] <-- +[%d]'):format(self:get_name(), name, value))
+	write_score(get_key(self) .. "+", name, value)
+end
+
+ac.game:event '游戏-结束' (function()
+	write_score("$", "GameEnd", 0)
+end)
