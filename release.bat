c3a937bbb596e6bb52e3b5bc1bb315bf293d4c59
最萌小汐 <sunyijiusyj@126.com>
2016-10-29 17:30:04 +0800

    修正无法打包为release版本的问题

--- release.bat
+++ release.bat
@@ -7,6 +7,6 @@ goto start
 :start
 
 del "%~dp0\map\lua\currentpath.lua"
-"%~dp0tools\w3x2txt\build\lua.exe" "%~dp0tools\make.lua" "release" "%userinput%"
+"%~dp0tools\w3x2txt\src\build\lua.exe" "%~dp0tools\make.lua" "release" "%userinput%"
 del "%~dp0\map\lua\currentpath.lua"
 pause
