ad5b83512c083e75446e39bb1c90c259833b9116
actboy168 <actboy168@gmail.com>
2017-05-18 14:56:31 +0800

    指定调试器版本

--- tools/运行.lua
+++ tools/运行.lua
@@ -3,9 +3,30 @@ require 'sys'
 if not ydwe then
     return
 end
+
+local function get_debugger()
+    local path = fs.path(os.getenv('USERPROFILE')) / '.vscode' / 'extensions'
+    for extpath in path:list_directory() do
+        if extpath:filename():string():sub(1, 20) == 'actboy168.lua-debug-' then
+            local dbgpath = extpath / 'debugger.dll'
+            if fs.exists(dbgpath) then
+                return dbgpath
+            end
+            return
+        end
+    end
+end
+
 local rootpath = fs.get(fs.DIR_EXE):remove_filename():remove_filename():remove_filename()
 local command = (registry.current_user() / [[SOFTWARE\Classes\YDWEMap\shell\run_war3\command]])['']
+command = command:gsub("%%1", (rootpath / 'MoeHero.w3x'):string())
+local dbg = get_debugger()
+if dbg then
+    command = command .. ' -debugger "' .. dbg:string() .. '"'
+end
+print(command)
 local p = sys.process()
-if p:create(nil, command:gsub("%%1", (rootpath / 'MoeHero.w3x'):string()), nil) then
+if p:create(nil, command, nil) then
     p:close()
 end
+
